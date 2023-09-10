aeece01365078aa492962da6b5dca2b5a98e728d
JDSerna00 <83099671+JDSerna00@users.noreply.github.com>
2023-09-09 22:37:25 -0500

    Some Classes Implementations

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    public Track[] tracks;
+
+    public void ModifyVol()
+    {
+
+    }
+
+    public void PauseMusic()
+    {
+
+    }
+
+    public void PlayTrack(string name)
+    {
+
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
