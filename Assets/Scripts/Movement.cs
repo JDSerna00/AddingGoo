f1c2a2fe92b8c3e7a6bc5c0e0d0eedc31f5d7e01
JDSerna00 <83099671+JDSerna00@users.noreply.github.com>
2023-10-15 22:06:25 -0500

    TestingColliders

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -4,32 +4,94 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
-    Rigidbody2D body;
+    public float moveSpeed = 1.0f;
+    public float wallSlideSpeed = 0.5f;
+    public float wallJumpForce = 5.0f;
+    public LayerMask wallLayer;
+    private float moveDirection = 0;
+    private bool isWallSliding = false;
+    private Rigidbody2D rb;
+    private bool isGrounded = false;
 
-    float horizontal;
-    float vertical;
-    int runSpeed = 20;
-    float moveLimiter = 0.7f;
-    // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
-        body = GetComponent<Rigidbody2D>();
+        rb = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        horizontal = Input.GetAxisRaw("Horizontal");
-        vertical = Input.GetAxisRaw("Vertical");
-    }
+        isGrounded = IsGrounded();
 
-    private void FixedUpdate()
-    {
-        if (horizontal != 0 && vertical != 0) 
-        {            
-            horizontal *= moveLimiter;
-            vertical *= moveLimiter;
+        moveDirection = Input.GetAxis("Horizontal");
+
+        isWallSliding = CheckWallSlide();
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            Jump();
         }
-        body.velocity = new Vector2(horizontal * runSpeed, vertical * runSpeed);
+
+        MoveCharacter();
+    }
+
+    private void MoveCharacter()
+    {
+        if (!isWallSliding)
+        {
+            Vector2 moveVelocity = rb.velocity;
+            moveVelocity.x = moveDirection * moveSpeed;
+            rb.velocity = moveVelocity;
+        }
+        else
+        {
+            Vector2 wallSlideVelocity = rb.velocity;
+            wallSlideVelocity.y = -wallSlideSpeed;
+            rb.velocity = wallSlideVelocity;
+        }
+
+        if (isGrounded)
+        {
+            rb.gravityScale = 1.0f;
+        }
+        else
+        {
+            rb.gravityScale = 0.0f;
+        }
+    }
+
+    private bool IsGrounded()
+    {
+        float checkDistance = 0.05f;
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.down, checkDistance, wallLayer);
+        return hit.collider != null;
+    }
+
+    private void Jump()
+    {
+        if (isWallSliding)
+        {
+            // Salto de la pared
+            Vector2 jumpForce = new Vector2(-Mathf.Sign(transform.localScale.x) * wallJumpForce, wallJumpForce);
+            rb.velocity = jumpForce;
+        }
+        else if (isGrounded)
+        {
+            // Salto normal
+            Vector2 jumpForce = new Vector2(0, wallJumpForce);
+            rb.velocity = jumpForce;
+        }
+    }
+
+    private bool CheckWallSlide()
+    {
+        float castDistance = 0.1f;
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.right * transform.localScale.x, castDistance, wallLayer);
+
+        if (hit.collider != null && !hit.collider.CompareTag("Ground"))
+        {
+            return true;
+        }
+
+        return false;
     }
 }
