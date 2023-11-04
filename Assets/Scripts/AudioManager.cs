1b7f1024c9c61ef0b0ff2c46789f7927d0364935
Artquake0 <sofia.gonzalezh@upb.edu.co>
2023-11-03 20:27:58 -0500

    BackgroundMusic

--- Assets/Scripts/AudioManager.cs
+++ Assets/Scripts/AudioManager.cs
@@ -1,29 +1,72 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class AudioManager : MonoBehaviour
 {
     public Track[] tracks;
+    public AudioSource audioSource;
+    public AudioClip Music1;
+    public AudioClip Music2;
+    public AudioClip Music3;
 
-    public void ModifyVol()
+    private void Start()
     {
+        ChangeSong(SceneManager.GetActiveScene(), LoadSceneMode.Single);
+    }
+
+
+    private void Enabled()
+    {
+
+        SceneManager.sceneLoaded += ChangeSong;
 
     }
 
-    public void PauseMusic()
+    private void Disabled()
     {
+
+        SceneManager.sceneLoaded -= ChangeSong;
 
     }
 
-    public void PlayTrack(string name)
+    
+    private void ChangeSong(Scene scene, LoadSceneMode mode)
     {
+        switch (scene.name)
+        {
+
+            case "Level1":
+                audioSource.clip = Music1;
+                break;
+
+            case "Level2":
+                audioSource.clip = Music2;
+                break;
+
+            case "Level3":
+                audioSource.clip = Music3;
+                break;
+
+        }
+
+        if (audioSource.clip != null)
+        {
+
+            audioSource.Play();
+
+        }
+
+
 
     }
+
+
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
-        
+        DontDestroyOnLoad(this.gameObject);
     }
 
     // Update is called once per frame
